7b8fc4ec7d6a2849a9642332102179343ecbe737
Sara Jimenez Carbajal <sara.jimenez@sngular.com>
2020-10-12 15:45:40 +0200

    enable wip pacts for the provider

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -31,9 +31,10 @@ describe("Pact Verification", () => {
     const fetchPactsDynamicallyOpts = {
       provider: "pactflow-example-provider",
       //consumerVersionTag: ['master', 'prod'], //the old way of specifying which pacts to verify
-      consumerVersionSelectors: [{ tag: 'feat/new-field', latest: true }, { tag: 'master', latest: true }, { tag: 'prod', latest: true } ], // the new way of specifying which pacts to verify
+      consumerVersionSelectors: [ {tag: process.env.TRAVIS_BRANCH, fallbackTag: 'master', latest: true }, { tag: 'prod', latest: true } ], // the new way of specifying which pacts to verify
       pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
-      enablePending: true
+      enablePending: true,
+      includeWipPactsSince: "2020-01-01"
     }
 
     const stateHandlers = {
